fa4ae6fbb977c990323b5138780efc7efd3c2ad1
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-02-12 22:11:18 +0000

    inital commit first draft

---
+++ main.js
@@ -0,0 +1,37 @@
+queue()
+    .defer(d3.csv, "file.csv")
+    .await(makeGraphs);
+
+function makeGraphs(error, data) {
+    var lineGraph = dc.lineChart('#chart-here');
+
+    var parseDate = d3.time.format("%d/%m/%Y %H:%M").parse;
+        data.forEach(function(d){
+            d.date = parseDate(d.date)
+        });
+    
+      var ndx = crossfilter(data);
+        var all = ndx.groupAll()
+        var ndxDimension = ndx.dimension((d) => d["date"]);
+        var ndxtemp1 = ndxDimension.group().reduceSum((d) => d["temp1"]);
+        var ndxtemp2 = ndxDimension.group().reduceSum((d) => d["temp2"]);
+        var ndxtemp3 = ndxDimension.group().reduceSum((d) => d["temp3"]);
+    
+    
+        var minDate = ndxDimension.bottom(1)[0].date;
+        var maxDate = ndxDimension.top(1)[0].date;
+
+    
+    lineGraph
+        .height(250)
+        .width(700)
+        .margins({ top: 10, right: 10, bottom: 20, left: 40 })
+        .x(d3.time.scale().domain([minDate,maxDate]))
+        .elasticX(true)
+        .brushOn(false)
+        .xAxisLabel("Date")
+        .yAxisLabel("Temperature")
+        .dimension(ndxDimension)
+        .group(ndxtemp3);
+    lineGraph.render();
+}
