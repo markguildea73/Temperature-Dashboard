de945ce98679c6c76c0cd838e04dc12777b99c8c
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-02-13 21:35:15 +0000

    trying different composite graph code

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+//try one
 queue()
     .defer(d3.csv, "file.csv")
     .await(makeGraphs);
 
 function makeGraphs(error, data) {
-    var lineGraph = dc.lineChart('#chart-here');
+    var lineGraph = dc.compositeChart('#chart-here');
 
     var parseDate = d3.time.format("%d/%m/%Y %H:%M").parse;
         data.forEach(function(d){
@@ -11,20 +12,17 @@ function makeGraphs(error, data) {
         });
     
       var ndx = crossfilter(data);
-        var all = ndx.groupAll()
         var ndxDimension = ndx.dimension((d) => d["date"]);
         var ndxtemp1 = ndxDimension.group().reduceSum((d) => d["temp1"]);
         var ndxtemp2 = ndxDimension.group().reduceSum((d) => d["temp2"]);
         var ndxtemp3 = ndxDimension.group().reduceSum((d) => d["temp3"]);
         
-        
-    
-    
+        //used the example on the lesson but the function is not appropriate for this
+       
         var minDate = ndxDimension.bottom(1)[0].date;
         var maxDate = ndxDimension.top(1)[0].date;
 
-    
-    lineGraph
+    compositeChart
         .height(250)
         .width(700)
         .margins({ top: 10, right: 10, bottom: 20, left: 40 })
@@ -34,7 +32,133 @@ function makeGraphs(error, data) {
         .xAxisLabel("Date")
         .yAxisLabel("Temperature")
         .dimension(ndxDimension)
-        .group(ndxtemp1),
- 
-    lineGraph.render();
+            .compose([
+                dc.lineChart(compositeChart)
+                .group(ndxtemp1),
+                dc.lineChart(compositeChart)
+                .group(ndxtemp2),
+                dc.lineChart(compositeChart)
+                .group(ndxtemp3),
+            ])
+            .render();
+        compositeChart.renderAll();
+        // used dc.renderAll(); no use
+        
+        
 }
+
+// removed this formorignal 
+//         .group(ndxtemp1),
+ 
+//     lineGraph.render();
+// }
+
+//Try 2
+// queue()
+//     .defer(d3.csv, "file.csv")
+//     .await(makeGraphs);
+
+// function makeGraphs(error, data) {
+
+
+//     var parseDate = d3.time.format("%d/%m/%Y %H:%M").parse;
+//         data.forEach(function(d){
+//             d.date = parseDate(d.date);
+//         });
+//         var minDate = ndxDimension.bottom(1)[0].date;
+//         var maxDate = ndxDimension.top(1)[0].date;
+    
+//         var ndx = crossfilter(data);
+        
+//         var ndxDimension = ndx.dimensionndx.dimension((d) => d["date"]);
+        
+//         var ndxtemp1 = ndxDimension.group().reduceSum((d) => d["temp1"]);
+            
+//         var ndxtemp2 = ndxDimension.group().reduceSum((d) => d["temp2"]);
+            
+//         var ndxtemp3 = ndxDimension.group().reduceSum((d) => d["temp3"]);
+            
+//         var compositeChart = dc.compositeChart('#chart-here');
+
+//         compositeChart
+//             .height(250)
+//             .width(700)
+//             .margins({ top: 10, right: 10, bottom: 20, left: 40 })
+//             .x(d3.time.scale().domain([minDate,maxDate]))
+//             .yAxisPadding(10)
+//             .brushOn(false)
+//             .xAxisLabel("Date")
+//             .yAxisLabel("Temperature")
+//             .dimension(ndxDimension)
+//             .compose([
+//                 dc.lineChart(compositeChart)
+//                 .group(ndxtemp1),
+//                 dc.lineChart(compositeChart)
+//                 .group(ndxtemp2),
+//                 dc.lineChart(compositeChart)
+//                 .group(ndxtemp3),
+//             ])
+//             .render();
+//         dc.renderAll();
+//     }
+
+//try three
+
+
+
+    queue()
+        .defer(d3.csv, "file.csv")
+        .await(makeGraphs);
+    function makeGraphs(error, data) {
+        var ndx = crossfilter(data);
+        var parseDate = d3.time.format("%d/%m/%Y").parse;
+        data.forEach(function(d){
+            d.date = parseDate(d.date);
+        });
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+        
+        //not appropriate for the file but will try
+        function temperature_logger_data(date) {
+            return function(d) {
+                if (d.temp1 === "temp1" ) {
+                    return +d.temp1;
+                } if(d.temp2 === "temp2" ) {
+                    return +d.temp2;
+                } if (d.temp1 === "temp3") {
+                    return +d.temp1;
+                } else {
+                    return 0;
+                }
+            };
+        }
+        var temp1Data = date_dim.group().reduceSum(temperature_logger_data('temp1'));
+        var temp2Data = date_dim.group().reduceSum(temperature_logger_data('temp2'));
+        var temp3Data = date_dim.group().reduceSum(temperature_logger_data('temp3'));
+        var compositeChart = dc.compositeChart('#composite-chart');
+        
+        compositeChart
+            .width(990)
+            .height(200)
+            .dimension(date_dim)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .yAxisLabel("Spend")
+            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .compose([
+                dc.lineChart(compositeChart)
+                    .colors('green')
+                    .group(temp1Data),
+                dc.lineChart(compositeChart)
+                    .colors('red')
+                    .group(temp2Data),
+                dc.lineChart(compositeChart)
+                    .colors('blue')
+                    .group(temp3Data),
+            ])
+            .brushOn(false)
+            .render();
+        dc.renderAll();
+    }
+
