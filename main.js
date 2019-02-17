8785623a4a5e97819ce3ee6c4411e4f78cb86f6b
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-02-17 21:29:27 +0000

    changes to the style and additional scatterchart

--- main.js
+++ main.js
@@ -18,36 +18,9 @@
         var temp2Data = date_dim.group().reduceSum(dc.pluck("temp2"));
         var temp3Data = date_dim.group().reduceSum(dc.pluck("temp3"));
         var compositeChart = dc.compositeChart('#chart-here');
-        var plot = dc.compositeChart('#plot-here');
         
         compositeChart
-            .width(990)
-            .height(200)
-            .dimension(date_dim)
-            .x(d3.time.scale().domain([minDate, maxDate]))
-            .yAxisLabel("Temperature")
-            .xAxisLabel("Date")
-            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-            .renderHorizontalGridLines(true)
-            .elasticX(false)
-            .yAxisPadding(5)
-            .compose([
-                dc.lineChart(compositeChart)
-                    .colors('green')
-                    .group(temp1Data, "temp1"),
-                dc.lineChart(compositeChart)
-                    .colors('red')
-                    .group(temp2Data, "temp2"),
-                dc.lineChart(compositeChart)
-                    .colors('blue')
-                    .group(temp3Data, "temp3"),
-            ])
-            .brushOn(false)
-            .render()
-            
-        // use this to trial scatter plot
-        plot
-            .width(990)
+            .width(450)
             .height(200)
             .dimension(date_dim)
             .x(d3.time.scale().domain([minDate, maxDate]))
@@ -71,6 +44,27 @@
             .brushOn(false)
             .render()
         
-        compositeChart.renderAll();
-        //used dc.renderAll() not working
-    }
+        var temp_dim = ndx.dimension(function (d) {
+            return [d.date, d.temp1];
+            })
+            
+            var temp_group = temp_dim.group();
+            var temp_chart = dc.scatterPlot("#plot-here");
+            
+            temp_chart
+                .width(450)
+                .height(200)
+                .x(d3.time.scale().domain([minDate, maxDate]))
+                .yAxisLabel("Temperature")
+                .xAxisLabel("Date")
+                .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+                .renderHorizontalGridLines(true)
+                .brushOn(false)
+                .symbolSize(2)
+                .clipPadding(10)
+                .dimension(temp_dim)
+                .group(temp_group, "Temp3")
+                
+                dc.renderAll()
+        }
+            
